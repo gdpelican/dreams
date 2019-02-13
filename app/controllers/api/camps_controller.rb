74f2b574668d33c552a70a8ecc9c74e2cd9f2ca9
James Kiesel <james.kiesel@gmail.com>
2019-02-14 08:21:36 +1300

    Copy changes

--- app/controllers/api/camps_controller.rb
+++ app/controllers/api/camps_controller.rb
@@ -10,8 +10,9 @@ class Api::CampsController < ApplicationController
   def index
     # NB, if we feel the need for more GraphQL-type functionality here, we can
     # do things like look at the request params and put them into the 'include' or
-    # 'fields' slots in the hash, which will allow the client to have pretty granular
-    # control over what data they receive back from the API.
+    # 'fields' slots in the hash, or filter results by time / status / etc,
+    # which will allow the client to have pretty granular control over what data
+    # they receive back from the API.
     render jsonapi: Camp.all,
            include: [:grants],
            fields: {
@@ -32,17 +33,19 @@ class Api::CampsController < ApplicationController
     # if @camp.save
     #   render jsonapi: @camp
     # else
-    #   render json: { errors: @camp.errors }
+    #   render json: { data: { errors: @camp.errors } }
     # end
   end
 
   private
 
-  # This isn't a super flexible method, but it's a quick way to get a little
+  # This isn't a super flexible method, but it's a quick and easy way to get a little
   # security onto what the API will hand out; essentially we put a master API key
   # into the ENV, and check it each time an API call is made, returning an error
   # if there's a mismatch. Note that leaving this ENV empty will allow _anyone_ to
   # make requests against the API... probably not the thing you want.
+  # So from here we simply generate a token of some kind, put that token into the ENV,
+  # and share that token with any apps who need access to this API.
   # (NB that this will want to go into a parent controller which runs this check
   # before each request)
   def verify_request!
